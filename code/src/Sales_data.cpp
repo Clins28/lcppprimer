f64351fb3e30e7e625ebd915d01bf70390170395
clins <clins28@126.com>
2018-05-17 17:12:29 +0800

    sln update 180517

--- code/src/Sales_data.cpp
+++ code/src/Sales_data.cpp
@@ -8,9 +8,10 @@ double Sales_data::avg_price() const		//const 把该函数定义为静态的成员函数
 		return 0;
 }
 
-Sales_data& Sales_data::combine(const Sales_data &rhs)
+Sales_data& Sales_data::combine(const Sales_data &rhs)	//函数返回左值
 {
 	units_sold += rhs.units_sold;
 	revenue += rhs.revenue;
-	return *this;
+	return *this;										//返回一个 this 指针的解引用，即执行该函数的对象
 }
+
