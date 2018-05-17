89730f3d2dae490c7832c843d159463adbbb697e
clins <clins28@126.com>
2018-05-17 11:23:41 +0800

    cpp change to sales_data

--- code/src/bookstore.cpp
+++ code/src/bookstore.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
-#include "..\h\Sales_item.h"
+#include "..\h\Sales_data.h"
+
+using namespace std;
 
 int main()
 {
-	Sales_item total; //保存下一条交易记录的变量
+	Sales_data total; //保存下一条交易记录的变量
 	//读入第一条交易记录，并确保有数据可以处理
-	if (std::cin >> total)
+	if (read(cin, total))
 	{
-		Sales_item trans;	//保存和的变量
+		Sales_data trans;	//保存和的变量
 		//读入并处理剩余交易记录
-		while(std::cin>>trans)
+		while(read(cin, trans))
 		{
 			//如果我们仍在处理相同的书
 			if (total.isbn() == trans.isbn())
-				total += trans;	//更新总销售额
+				total.combine(trans);	//更新总销售额
 			else
 			{
 				//打印前一本书的结果
-				std::cout << total << std::endl;
+				cout << total << endl;
 				total = trans;	//total现在表示下一本书的销售额
 			}
 		}
-		std::cout << total << std::endl;	//打印最后一本书的结果
+		cout << total << endl;	//打印最后一本书的结果
 	}
 	else
 	{
 		//没有输入！ 警告读者
-		std::cerr << "No data?!" << std::endl;
+		cerr << "No data?!" << endl;
 		return -1;	//表示失败
 	}
 	return 0;
