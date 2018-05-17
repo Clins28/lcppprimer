4030849a2e2761353e61de8d60c0951e8c281e35
clins <clins28@126.com>
2018-05-17 10:55:53 +0800

    origin submit

---
+++ code/h/Sales_data.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#ifndef SALESDATA_H
+#define SALESDATA_H
+using namespace std;
+using std::string;
+
+class Sales_data
+{
+private:
+	string bookNo = "";
+	unsigned int units_sold = 0;
+	double revenue = 0.0;
+
+
+};
+
+#endif
